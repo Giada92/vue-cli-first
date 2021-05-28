15301a383eb8a032f8d5e14ffeca9a39b4dc6267
Giada92 <giada_meloni@virgilio.it>
2021-05-28 15:34:25 +0200

    Creato un titolo in pagina

---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+h1 {
+    color: red;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+</style>
